e2516f1d292884437cfc4ec63a796bc82bcfc1fc
Amit Chaman <amit.chaman@neosoftmail.com>
2023-04-27 11:32:43 +0530

    changes done

---
+++ app.js
@@ -0,0 +1,25 @@
+window.onload = () => {
+    const buttons =  document.querySelectorAll('.numbers') ;
+    const screen = document.querySelector('.screen');
+    const equals = document.querySelector('#btn-equals'); 
+    const clear = document.querySelector('#btn-clear');  
+    buttons.forEach(button => {
+        button.addEventListener('click' , (e) => {
+              console.log(e.target.innerHTML);
+              screen.value += e.target.innerHTML;
+        })
+        equals.addEventListener('click' , (e) => {
+            if(!screen.value) 
+            {
+                screen.value = "Enter value"
+            }
+            else {
+                screen.value = eval(screen.value);
+            }
+        })
+        clear.addEventListener('click' , (e) => {
+            screen.value= ""
+        })
+    })
+
+}
